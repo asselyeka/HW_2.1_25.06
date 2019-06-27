ce01d537af87c01ced7b76d660b0aad73f75edfa
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 23:56:20 +0600

    Добавьте файлы проекта.

---
+++ HW_2.1_25.06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.	Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.
+
+namespace HW_2._1_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1;
+            var y = 4;
+            var z = 5;
+            Console.Write(x);
+            Console.Write("  ");
+            Console.Write(y);
+            Console.Write("  ");
+            Console.Write(z);
+            Console.ReadKey();
+        }
+    }
+}
